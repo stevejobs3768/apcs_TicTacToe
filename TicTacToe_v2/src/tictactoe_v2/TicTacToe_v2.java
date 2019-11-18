6c21168f91ceac766d6857c373441cc2958800a5
Ishan Madan <ishan@mistlogic.com>
2019-11-18 13:59:11 -0800

    edited the v2 project, added how to win paper and victory chart

--- TicTacToe_v2/src/tictactoe_v2/TicTacToe_v2.java
+++ TicTacToe_v2/src/tictactoe_v2/TicTacToe_v2.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tictactoe_v2;
 
 /**
- *
  * @author ishanmadan
  */
 public class TicTacToe_v2 {
@@ -15,7 +9,6 @@ public class TicTacToe_v2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
